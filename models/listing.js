1b496478adb4dc66815f0a405cb2bf09dc76f9d9
sparsh singh chundawat <singhsparsh2003@gmail.com>
2024-08-05 15:36:37 +0530

    Adding Review Feature - Frontend

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const reviews = require("./reviews");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -17,6 +18,12 @@ const listingSchema = new Schema({
   price: Number,
   location: String,
   country: String,
+  reviews : [
+    {
+      type: Schema.Types.ObjectId,
+      ref : "Review",
+    },
+  ]
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
